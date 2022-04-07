f2704ddbe8745fe4599049282688219c61031c0f
Daniel Belrod <omard.beltrano@ecci.edu.co>
2022-04-07 00:16:54 -0500

    configuración del proyecto, instalación de dependencias e implementación de los elementos instalados como babel, webpack, plugin y creación de scripts

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const App = () => {
+    return {
+
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('app')
+);
